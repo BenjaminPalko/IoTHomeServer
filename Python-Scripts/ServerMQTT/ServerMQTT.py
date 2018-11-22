be41c4ab69e54c84e6cf105e6e4e7a0f61046153
Benjamin Palko <benjaminpalko@hotmail.com>
2018-11-22 13:22:32 -0500

    Console UI + MQTT
    MQTT controller
    Console UI

--- Python-Scripts/ServerMQTT/ServerMQTT.py
+++ Python-Scripts/ServerMQTT/ServerMQTT.py
@@ -1,29 +1,89 @@
-# Benjamin Palko 100964652
-import time
-import paho.mqtt.client as mqtt
+import sys
+import logging as Logger
+import ClientController
 
-topics = []
+# Helper variables, functions & logging
+main_commands = [ 'client', 'exit' ]
 
+def client_loop():
+    client_commands = [ 'list', 'create', 'start', 'stop', 'publish' ]
+    print('\nClient Interface --Help')
+    while(True):
+        command = input('>>')
 
-# Passed to client as default 'on_connect' function
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: " + str(rc))
+        if command == '':
+            pass
+        # Help for client interface
+        elif command.lower() == 'help':
+            print(', '.join(client_commands))
+        # List Idle & Running Clients
+        elif command.lower() == 'list':
+            [idle, running] = ClientController.get_clients()
+            print('Idle Clients:')
+            print(', '.join(idle))
+            print('Running Clients:')
+            print(', '.join(running))
+        # Create a new client
+        elif command.lower() == 'create':
+            name = input('Name: ')
+            broker = input('Broker address: ')
+            topics = input('(Space Delimited) Topics: ').split(" ")
+            if broker == '' or broker == None:
+                if ClientController.create_client(name, topics) == 0:
+                    print(name + ' created...')
+                else:
+                    print('Unable to create client...')
+            else:
+                if ClientController.create_client(name, topics, broker) == 0:
+                    print(name + ' created...')
+                else:
+                    print('Unable to create client...')
+        # Start an idle client
+        elif command.lower() == 'start':
+            name = input('Client ID:')
+            if ClientController.start_client(name):
+                print(name + ' started...')
+            else:
+                print('Error occured!')
+        # Stop running client
+        elif command.lower() == 'stop':
+            name = input('Client ID:')
+            if ClientController.stop_client(name):
+                print(name + ' stopped...')
+            else:
+                print('Error occured!')
+        # Publish using given client
+        elif command.lower() == 'publish':
+            name = input('Client ID:')
+            topic = input('Topic:')
+            msg = input('Message:')
+            if ClientController.publish_client(name, topic, msg):
+                print('Publish Successful')
+            else:
+                print('Publish Failure')
 
+        # Exit client interface
+        elif command.lower() == 'exit':
+            break
+        else:
+            print("Command '" + command + "' not recognized")    
 
-# Passed to client as default 'on_message' function
-def on_message(client, userdata, msg):
-    if msg.topics in topics:
-        print(msg.topic + " " + str(msg.payload))
+# Program start
+print('Server Interface Started --Help')
+while(True):
+    command = input('>>')
 
+    # Parse input
+    if command == '':
+        pass
+    elif command.lower() == "help":
+        print(', '.join(main_commands))
+    elif command.lower() == "client":
+        client_loop()
+    elif command.lower() == "exit":
+        break
+    else:
+        print("Command '" + command + "' not recognized")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect('192.168.1.215', 1883, 60)
-
-client.loop_start()
-
-print('Script is running, Ctrl-C to quit...')
-while True:
-    time.sleep(0.2)
+# Hit end of program, exit
+sys.exit(0)
