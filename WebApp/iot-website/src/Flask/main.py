019cb870a94dfae2b124d95f86692550567c57ff
Unknown <andrewdlqnguyen@gmail.com>
2019-01-21 22:36:14 -0500

    Prototype browser html control on LED

--- WebApp/iot-website/src/Flask/main.py
+++ WebApp/iot-website/src/Flask/main.py
@@ -1,27 +1,47 @@
 from flask import Flask, request, render_template
+from rgbledcontroller import *
 
 app = Flask(__name__)
 
-@app.route("/profile/<name>")
-def profile(name):
-    return render_template("profile.html", name=name)
-
-@app.route("/") # for null input
-@app.route("/<user>")
-def index(user="nothing"):
-    return render_template("user.html", user=user)
-
-@app.route("/objects") 
-def objectPassing():
-    objects = ["object1", "object2", "object3"]
-    return render_template("objectList.html", objects = objects)
-
 @app.route("/index")
-def reactHTML():
+def redon():
+    # red_on()
     return render_template("index.html")
+
+# @app.route("/red_on", methods=['POST'])
+# def redon():
+#     red_on()
+#     return render_template("index.html")
+
+@app.route('/<led_setting>')
+def testing(led_setting):
+    if led_setting == "red":
+        red_on()
+    if led_setting == "blue":
+        blue_on()
+    if led_setting == "green":
+        green_on()
+    if led_setting == "off":
+        led_off()
+    return render_template("index.html", setting=led_setting)
 
 if __name__== "__main__":
     app.run(debug=True)
+
+# @app.route("/profile/<name>")
+# def profile(name):
+#     return render_template("profile.html", name=name)
+
+# @app.route("/") # for null input
+# @app.route("/<user>")
+# def index(user="nothing"):
+#     return render_template("user.html", user=user)
+
+# @app.route("/objects") 
+# def objectPassing():
+#     objects = ["object1", "object2", "object3"]
+#     return render_template("objectList.html", objects = objects)
+
 # # @This is a decorator - wrap a function and modify its behavior
 # @app.route("/prototype") #Mapped a URL to return vaule ex/prototype
 # def prototype():
