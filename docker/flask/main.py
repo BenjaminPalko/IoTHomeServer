46402f7eb2f3d8a1c66d8cfb930dbacd3dc2d974
Benjamin <benjaminpalko@hotmail.com>
2019-03-09 10:08:19 -0500

    Ignore .idea and changes to travis.yml

--- docker/flask/main.py
+++ docker/flask/main.py
@@ -12,10 +12,11 @@ app.config.update(
     SQLALCHEMY_PASSWORD='root'
 )
 
+
 @app.route('/')
 def default():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app)
