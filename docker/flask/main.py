af290318b60854ba71dd2704396b2855405fecd6
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-03-13 22:22:59 -0400

    Socket working for device. Thread not yet working

--- docker/flask/main.py
+++ docker/flask/main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO
-from flask_mqtt import Mqtt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy.orm import relationship, backref
 import os
-import json
+import time
+import _thread
+
 
 RGB= '80:7D:3A:3C:54:5B'
 TEMP= '80:7D:3A:3C:57:60'
@@ -15,7 +15,6 @@ WEATHER= '80:7D:3A:6E:96:78'
 DOORLOCK= '80:7D:3A:6E:88:0D'
 DOCKER_BROKER = '0.0.0.0'
 security_pin = None
-dateTime = datetime.now()
 
 
 # Flask + Socket + MQTT Setup
@@ -25,12 +24,8 @@ app.config.update(
     SQLALCHEMY_DATABASE_URI=os.environ['POSTGRES_DB'],
     SQLALCHEMY_USERNAME=os.environ['POSTGRES_USER'],
     SQLALCHEMY_PASSWORD=os.environ['POSTGRES_PASSWORD']
-    # MQTT_BROKER_URL=AWS_MOSQUITTO,
-    # MQTT_BROKER_PORt=1883,
-    # MQTT_KEEPALIVE=60
 )
 socketio = SocketIO(app)
-# mqtt = Mqtt(app)
 db = SQLAlchemy(app)
 engine = create_engine(os.environ['POSTGRES_DB'], convert_unicode=True, echo=False)
 
@@ -88,148 +83,55 @@ class DoorLock(Base, db.Model):
         return f"DoorLock('{self.id}', '{self.pin}', '{self.timestamp}')"
 
 
-# MQTT Subscriptions (Temporary, try to remove at earliest possibility)
-# mqtt.subscribe('nodemcu/temp')
-# mqtt.subscribe('nodemcu/doorlock/pub')
-
-
-# PIN Device variable (Temporary, try to remove at earliest possibility)
-security_pin = 1232
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def default():
-
-    if request.method =='POST':
-        if "colorpicked" in request.form:
-            f = request.form['ledSwitch']
-            newRGB_led(f)
-        if "pinSet" in request.form:
-            f = request.form['pinCombo']
-            newSet_pin(f)
-        if "forecastSet" in request.form:
-            f = request.form['forecast']
-            newForecast(f)
-
     return render_template("index.html")
-
-
-# RGBLED
-# Receive rgb Hex value from client
-@socketio.on('rgbled_hex')
-def update_rgb_led(rgbled_hex):
-    rgbled_hex = str(rgbled_hex).replace('#', '')
-
-    rgbled = RgbLED(color=rgbled_hex, change=True)
-    db.session.add(rgbled)
-    db.session.commit()
-    print("data: " + rgbled)
 
 
 # Weather Display
 # Receive location string from client
 @socketio.on('weather_location')
-def change_weather_display(weather_location):
-    print("testing connection")
-    json_string = scripts.api_handler.retrieve_weather(weather_location)
-    parsed_string = scripts.api_handler.parse_weather_data(json_string)
-
-    weatherlocation = WeatherDisplay(location=parsed_string, change=True)
-    db.session.add(weatherlocation)
-    db.session.commit()
-    print("data: " + weatherlocation)
-
-
-# Door Lock
-# Set pin (Finish)
-@socketio.on('door_pin')
-def set_pet(door_pin):
-    global security_pin
-    security_pin = str(door_pin)
-
-    doorlock = DoorLock.query.filter_by(id=DOORLOCK).first()
-    doorlock.pin = security_pin
-    db.session.commit()
-
-    print(security_pin)
-
-
 def newForecast(value):
     weather = WeatherDisplay.query.filter_by(id=WEATHER).first()
     weather.location = value
+    weather.timestamp = datetime.now()
+    weather.change = True
     db.session.commit()
 
 
+# RGBLED
+# Receive rgb Hex value from client
+@socketio.on('rgbled_hex')
 def newRGB_led(value):
     rgbled = RgbLED.query.filter_by(id=RGB).first()
     rgbled.color = value
+    rgbled.timestamp = datetime.now()
+    rgbled.change = True
     db.session.commit()
 
 
+# Doorlock
+# Set doorlock for device
+@socketio.on('door_pin')
 def newSet_pin(value):
-    global security_pin
-    security_pin = str(value)
-
     doorlock = DoorLock.query.filter_by(id=DOORLOCK).first()
-    doorlock.pin = security_pin
+    doorlock.pin = value
+    doorlock.timestamp = datetime.now()
     db.session.commit()
+
 
 # Temperature
 # Send new temp received from MQTT to client
 # Retrieving the data from Database
-def update_temperature():
-    myTemperature = TemperatureSensor.query.order_by(TemperatureSensor.id.desc()).first()
-    socketio.emit('temperature', myTemperature.value)
-
-
-# # <<<   Device Socket Methods  >>>
-# # Temperature
-# # Send new temp received from MQTT to client
-# @mqtt.on_topic('nodemcu/temp')
-# def update_temperature(client, userdata, message):
-#     json_object = json.loads(message.payload.decode())
-#     temp = json_object['temperature']
-#     socketio.emit('temperature', str(round(temp, 1)))
-#
-#
-# # RGBLED
-# # Receive rgb Hex value from client
-# @socketio.on('rgbled_hex')
-# def update_rgb_led(rgbled_hex):
-#     rgbled_hex = str(rgbled_hex).replace('#', '')
-#     mqtt.publish('nodemcu/rgbled', rgbled_hex)
-#
-#
-# # Weather Display
-# # Receive location string from client
-# @socketio.on('weather_location')
-# def change_weather_display(weather_location):
-#     json_string = scripts.api_handler.retrieve_weather(weather_location)
-#     parsed_string = scripts.api_handler.parse_weather_data(json_string)
-#     mqtt.publish('nodemcu/weather', parsed_string)
-#
-#
-# # Door Lock
-# # Set pin (Finish)
-# @socketio.on('door_pin')
-# def set_pet(door_pin):
-#     global security_pin
-#     security_pin = str(door_pin)
-#     print(security_pin)
-#
-#
-# # Check pin (Finish)
-# @mqtt.on_topic('nodemcu/doorlock/pub')
-# def check_pin(client, userdata, message):
-#     print(message)
-#     json_object = json.loads(message.payload.decode())
-#     if security_pin == str(json_object["Passcode"]):
-#         return_json = {"validation": 1}
-#     else:
-#         return_json = {"validation": 0}
-#     print(return_json)
-#     mqtt.publish('nodemcu/doorlock/sub', json.dumps(return_json))
+def update_temperature_loop():
+    print('Thread connected')
+    while True:
+        time.sleep(2)
+        # myTemperature = TemperatureSensor.query.order_by(TemperatureSensor.timestamp.desc()).first()
+        myTemperature = TemperatureSensor.query.filter_by(id=TEMP).first()
+        socketio.emit('temperature', myTemperature.value)
 
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
+    _thread.start_new_thread(update_temperature_loop(), ('',))
