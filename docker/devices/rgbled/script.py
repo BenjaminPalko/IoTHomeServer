6b9e09f0ad03e9471d9cfb36deabb83fec7b4656
benjaminpalko@gmail.com <benjaminpalko@gmail.com>
2019-03-14 21:35:11 -0400

    Added logging

--- docker/devices/rgbled/script.py
+++ docker/devices/rgbled/script.py
@@ -1,21 +1,22 @@
 from paho.mqtt import client
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, event
+from datetime import datetime
+import logging
 import json
 import time
 import os
+import sys
 
 #   Device Id
 device_mac = os.environ['MAC_ADDRESS']
 broker = os.environ['MQTT_BROKER']
 topic = os.environ['MQTT_TOPIC']
 db_url = os.environ['POSTGRES_URL']
-db_password = os.environ['POSTGRES_PASSWORD']
-
 
 '''
     POSTGRES Database
 '''
-engine = create_engine(db_url, echo=True)
+engine = create_engine(db_url)
 connection = engine.connect()
 
 
@@ -29,32 +30,68 @@ connect_results = {
     3: "Connection refused - bad username or password",
     4: "Connection refused - not authorised"
 }
-client = client.Client()
 
 
 def on_connect(client, userdata, flags, rc):
-    print(connect_results[rc])
+    logging.info(connect_results[rc])
 
 
-client.connect(broker, 1883)
+#   Broker connection
+client = client.Client()
+client.on_connect = on_connect
+while client.connect(broker, 1883):
+    logging.warning('Broker failed to connect, attempting to reconnect in 4 seconds...')
+    time.sleep(4)
 client.subscribe(topic)
 client.loop_start()
 
 
-def check_update():
+def check_value():
     try:
-        query = text("select * from rgb_led where id = :mac")
-        result = connection.execute(query, mac=device_mac).fetchone()
+        logging.debug('Checking for new value')
+        device_query = text("select * from rgb_led where id = :mac")
+        result = connection.execute(device_query, mac=device_mac).fetchone()
         if result is not None and result[3]:
-            query2 = text("update rgb_led set change = FALSE where id = :mac")
-            connection.execute(query2, mac=device_mac)
-            json_object = {"mac": device_mac, "data": {"color": result[1]}}
-            client.publish(topic, json.dumps(json_object))
-            print('Message sent')
+            try:
+                logging.debug('Query Data for RGB hex value')
+                hex_query = text("update rgb_led set change = FALSE where id = :mac")
+                connection.execute(hex_query, mac=device_mac)
+                json_string = json.dumps({"mac": device_mac, "data": {"color": result[1]}})
+                client.publish(topic, json.dumps(json_string))
+                logging.info('New Value sent:' + result[1])
+            except TypeError:
+                logging.error('SQL query error on value retrieval')
     except TypeError:
-        pass
+        logging.error('SQL query error on check for new value')
 
 
-while True:
-    check_update()
-    time.sleep(5)
+def main():
+    #   Logging setup
+    logging_level = logging.INFO
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging_level)
+    stream_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('./logs/{:%Y-%m-%d}.log'.format(datetime.now()))
+    file_handler.setLevel(logging_level)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+    logging.info('Program Started')
+    #   Start execution loop
+    while True:
+        check_value()
+        time.sleep(2)
+
+    client.loop_stop()
+    logging.debug('Program exiting')
+    time.sleep(2)
+
+
+main()
